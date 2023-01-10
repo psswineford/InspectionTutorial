954cbdf805356178b5fa7093ffb24c04f348f89b
Shawn Swineford <psswineford@gmail.com>
2023-01-10 15:24:06 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InspectionComponent } from './components/inspection/inspection.component';
+import { ShowInspectionComponent } from './components/inspection/show-inspection/show-inspection.component';
+import { AddEditInspectionComponent } from './components/inspection/add-edit-inspection/add-edit-inspection.component';
+import { InspectionApiService } from './services/inspection-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InspectionComponent,
+    ShowInspectionComponent,
+    AddEditInspectionComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [InspectionApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
